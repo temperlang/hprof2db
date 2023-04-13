eda4984f7073f9fa2b8ecac10a9fc0c717e70a4b
Tom <tom@temper.systems>
2023-04-13 11:53:07 -0700

    Start sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
+use anyhow::Result;
 use jvm_hprof::{heap_dump::SubRecord, parse_hprof, HeapDumpSegment, RecordTag};
-use std::{env, fs, io};
+use rusqlite::Connection;
+use std::{env, fs};
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let path = args[1].as_str();
-    println!("Hello, {path}!");
+    println!("Reading: {path}");
+    let conn = Connection::open_in_memory()?;
+    build_schema(&conn)?;
     parse_records(fs::File::open(path)?);
+    Ok(())
+}
+
+fn build_schema(conn: &Connection) -> Result<()> {
+    conn.execute(
+        "
+        create table instance (
+            id integer primary key,
+            class_id integer
+        )
+        ",
+        (),
+    )?;
     Ok(())
 }
 
@@ -20,7 +37,8 @@ fn parse_records(file: fs::File) {
         match record.tag() {
             RecordTag::HeapDumpSegment => {
                 dump_count += 1;
-                instance_count += parse_dump_records(&record.as_heap_dump_segment().unwrap().unwrap())
+                instance_count +=
+                    parse_dump_records(&record.as_heap_dump_segment().unwrap().unwrap())
             }
             _ => {}
         }
